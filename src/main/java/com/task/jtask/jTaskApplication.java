4b9b425ad8d030a3ec5be8f821df4b94e12a9145
g1tPush <109822566+g1tPush@users.noreply.github.com>
2024-08-04 00:03:43 +0300

    initial commit

---
+++ src/main/java/com/task/jtask/jTaskApplication.java
@@ -0,0 +1,13 @@
+package com.task.jtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class jTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(jTaskApplication.class, args);
+	}
+
+}
