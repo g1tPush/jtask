6103e5376096139a2ceffd2bfb034921fd875126
g1tPush <109822566+g1tPush@users.noreply.github.com>
2024-08-04 15:19:24 +0300

    refactor: add RestTemplate to config

---
+++ src/main/java/com/task/jtask/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.task.jtask.config;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
