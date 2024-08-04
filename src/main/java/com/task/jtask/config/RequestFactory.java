64d27de10a1688151b91cdf3a059fe0f1f958e42
g1tPush <109822566+g1tPush@users.noreply.github.com>
2024-08-04 15:20:01 +0300

    refactor: create RequestFactory for HttpEntity configuration of translation request

---
+++ src/main/java/com/task/jtask/config/RequestFactory.java
@@ -0,0 +1,41 @@
+package com.task.jtask.config;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.task.jtask.dto.TranslationDto;
+import com.task.jtask.request.TranslationRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RequestFactory {
+
+    private final ApiConfig apiConfig;
+    private final ObjectMapper objectMapper;
+
+    public RequestFactory(ApiConfig apiConfig, ObjectMapper objectMapper) {
+        this.apiConfig = apiConfig;
+        this.objectMapper = objectMapper;
+    }
+
+    public HttpEntity<String> createTranslationRequestEntity(TranslationDto translationDto) throws JsonProcessingException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Api-Key " + apiConfig.getApiKey());
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        TranslationRequest translationRequest = new TranslationRequest(
+                apiConfig.getFolderId(),
+                List.of(translationDto.getText()),
+                translationDto.getTargetLanguageCode(),
+                translationDto.getSourceLanguageCode()
+        );
+
+        String requestBody = objectMapper.writeValueAsString(translationRequest);
+
+        return new HttpEntity<>(requestBody, headers);
+    }
+}
