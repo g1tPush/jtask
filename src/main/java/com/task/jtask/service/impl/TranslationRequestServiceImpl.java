2e964a0f6bace11220f61952624f741da3d1368f
g1tPush <109822566+g1tPush@users.noreply.github.com>
2024-08-04 15:21:11 +0300

    refactor: refactor TranslationRequestServiceImpl

--- src/main/java/com/task/jtask/service/impl/TranslationRequestServiceImpl.java
+++ src/main/java/com/task/jtask/service/impl/TranslationRequestServiceImpl.java
@@ -1,46 +1,32 @@
 package com.task.jtask.service.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.task.jtask.config.ApiConfig;
+import com.task.jtask.config.RequestFactory;
 import com.task.jtask.dto.TranslationDto;
-import com.task.jtask.request.TranslationRequest;
 import com.task.jtask.response.TranslationResponse;
 import com.task.jtask.service.TranslationRequestService;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @Service
 public class TranslationRequestServiceImpl implements TranslationRequestService {
 
     private final ApiConfig apiConfig;
-    RestTemplate restTemplate = new RestTemplate();
+    private final RequestFactory requestFactory;
+    private final RestTemplate restTemplate;
 
-    TranslationRequestServiceImpl(ApiConfig apiConfig) {
+    TranslationRequestServiceImpl(ApiConfig apiConfig, RequestFactory requestFactory,RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+        this.requestFactory = requestFactory;
         this.apiConfig = apiConfig;
     }
 
     public TranslationResponse translate(TranslationDto translationDto) throws JsonProcessingException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Api-Key " + apiConfig.getApiKey());
-
-        TranslationRequest translationRequest = new TranslationRequest(
-                apiConfig.getFolderId(),
-                List.of(translationDto.getText()),
-                translationDto.getTargetLanguageCode(),
-                translationDto.getSourceLanguageCode()
-        );
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper.writeValueAsString(translationRequest);
-
-        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+        HttpEntity<String> entity = requestFactory.createTranslationRequestEntity(translationDto);
 
         ResponseEntity<TranslationResponse> response = restTemplate.exchange(
                 apiConfig.URL,
@@ -49,8 +35,6 @@ public class TranslationRequestServiceImpl implements TranslationRequestService 
                 TranslationResponse.class
         );
 
-        TranslationResponse translationResponse = response.getBody();
-
-        return translationResponse;
+        return response.getBody();
     }
 }
