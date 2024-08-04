805ba31cc531dd1856de3c21b1e4c13d1c6644c1
g1tPush <109822566+g1tPush@users.noreply.github.com>
2024-08-04 14:49:09 +0300

    feat: add translation request service interface

---
+++ src/main/java/com/task/jtask/service/TranslationRequestService.java
@@ -0,0 +1,9 @@
+package com.task.jtask.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.task.jtask.dto.TranslationDto;
+import com.task.jtask.response.TranslationResponse;
+
+public interface TranslationRequestService {
+    TranslationResponse translate(TranslationDto translationDto) throws JsonProcessingException;
+}
